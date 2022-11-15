31e069d2d20091d2b0b5ce31761dd68d2669cb55
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-11-15 09:42:18 -0800

    working through syntax error - redoing seeding

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -1,8 +1,22 @@
-const setup = require('../../data/setup');
-const pool = require('../utils/pool');
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('Books test routes', () => {
+describe('books routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+  test('/books returns list', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(6);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      year: expect.any(Number),
+    });
+  });
+  afterAll(() => {
+    pool.end();
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,30 +1,50 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
--- books_authorS?
-DROP TABLE IF EXISTS authors;
-DROP TABLE IF EXISTS books;
+DROP table if exists books_authors;
+DROP table if exists books;
+Drop table if exists authors;
 
-CREATE TABLE authors (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  first_name VARCHAR not NULL,
-  country VARCHAR
-)
-
-INSERt inTo authors (first_name, country) VALUEs
-('Georgia', 'Romania')
-('SAMUELSONTIN', 'Papa New Guinea')
-('Shorsh', 'Mexico')
-('Rauf', 'Tajikistan');
-
-CREATE TABLE books (
+CREATE table books (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   title VARCHAR NOT NULL,
-  year INT NOT NULL,
-)
+  released INT NOT NULL
+);
 
-INSERt IntO books (title, year) VALUEs
+INSERT INTO books (title, released) VALUES
 ('How the West became East', 2912),
 ('WHAT WILL HAPPEN IN THE YEAR 2000', 2001),
 ('TO WRITE A BOOK', 1802),
+('The Chungus Amungus', 1934),
 ('A Mycologists guide to Chess', 2011),
-('A Fishermens Guide to Darts', 1010);
+('A Fishermens Guide to Darts', 2010);
+
+
+CREATE table authors (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  dob DATE Not NUll,
+  pod VARCHAR
+);
+
+INSERT INTO authors (name, dob, pod) VALUES
+('Rauf Tapatio', '1988', 'Canada'), 
+('Miquel', '1857', 'Mazatlan'),
+('Abalone Simpson', '2998', 'MAINE'),
+('Neil River', '1966', 'Xocitil'),
+('Terrie Cruise', '1745', 'Uzbekistan');
+
+
+CREATE table books_authors (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  book_id INT,
+  author_id INT,
+  FOREIGN KEY (book_id) REFERENCES books(id),
+  FOREIGN KEY (author_id) REFERENCES authors(id)
+);
+
+INSERT INTO books_authors(book_id, author_id) VALUES
+(1, 1),
+(2, 1),
+(3, 2),
+(4, 2),
+(5, 3),
+(6, 4),
+(6, 5);

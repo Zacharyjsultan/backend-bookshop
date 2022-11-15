c9f3e92167fd7c1764046328baddcff0dc50f185
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-11-15 10:14:01 -0800

    2/4 for bilbo by :id

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -15,6 +15,15 @@ describe('books routes', () => {
       released: expect.any(Number),
     });
   });
+  test('/books/:id returns auth details', async () => {
+    const res = await request(app).get('/books/1');
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      released: expect.any(Number),
+      authors: expect.any(Array),
+    });
+  });
   afterAll(() => {
     pool.end();
   });
